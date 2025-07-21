937746a0fbbf4e9b5ef74b798dfe7a6790f0d6c9
Sai Krishna S <saikrishnas.yadav@gmail.com>
2025-07-21 16:41:16 +0000

    container config done

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score=false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn=1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1=>true;0=>false
+// "hitesh"=>true
+// ""=>false
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
