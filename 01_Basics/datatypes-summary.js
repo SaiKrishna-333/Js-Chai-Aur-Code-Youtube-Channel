8e4cbc0c0d3b80a52a93ba65bce714ea91be1444
Sai Krishna S <saikrishnas.yadav@gmail.com>
2025-07-21 17:47:39 +0000

    container config done

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id===anotherId)
+
+
+// const bigNumber = 123456789987654321n
+
+
+//Referrence (Non primitive)
+
+//Arrays , Objects , Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+
+let myObj = {
+    name : "Sai",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello Worls")
+}
+
+console.log(typeof myFunction)
