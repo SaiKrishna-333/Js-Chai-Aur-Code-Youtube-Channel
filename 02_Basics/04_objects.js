f19c3a0bfb76008cb1fdb6f4cc34da55e7c396ae
Sai Krishna S <saikrishnas.yadav@gmail.com>
2025-07-22 16:24:26 +0000

    container config done

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -19,5 +19,36 @@ const regularUser={
 const obj1 = {1:"a", 2:"b"}
 const obj2 = {3:"a", 4:"b"}
 // const obj3 = {obj1,obj2}
-const obj3 = Object.assign({},obj1,obj2)
-console.log(obj3)
+// const obj3 = Object.assign({},obj1,obj2)
+const obj3={...obj1,...obj2}
+// console.log(obj3)
+
+
+
+const users = [
+    {
+        id: 1,
+        email: "SaiKrishna@gmail.com"
+    },
+    {
+        id: 1,
+        email: "SaiKrishna@gmail.com"
+    },
+    {
+        id: 1,
+        email: "SaiKrishna@gmail.com"
+    },
+    {
+        id: 1,
+        email: "SaiKrishna@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
